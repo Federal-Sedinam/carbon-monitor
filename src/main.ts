2e4095ff3ffc21e3a7f98ea921e88162aa4c88aa
Federal-Sedinam <federalsedinam@gmail.com>
2026-09-04 14:44:40 +0100

    feat: show current national carbon intensity

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-import './style.css'
+import "./style.css";
+
+const response = await fetch("https://api.carbonintensity.org.uk/intensity");
+const body = await response.json();
+const reading = body.data[0].intensity;
+
+document.querySelector("#app")!.innerHTML = `
+  <p class="reading">
+    <span class="reading__value">${reading.actual}</span>
+    <span class="reading__unit">gCO₂/kWh</span>
+  </p>
+`;
