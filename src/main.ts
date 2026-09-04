e6a5a1d91deba25d4d8fd5328acfabb2c1a524ec
Federal-Sedinam <federalsedinam@gmail.com>
2026-09-04 16:56:05 +0100

    feat: show loading and error states while fetching

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import "./style.css";
+import './style.css'
 
-const response = await fetch("https://api.carbonintensity.org.uk/intensity");
-const body = await response.json();
-const reading = body.data[0].intensity;
+const app = document.querySelector('#app')!
 
-document.querySelector("#app")!.innerHTML = `
-  <p class="reading">
-    <span class="reading__value">${reading.actual}</span>
-    <span class="reading__unit">gCO₂/kWh</span>
-  </p>
-`;
+app.innerHTML = `<p class="reading">Loading…</p>`
+
+try {
+  const response = await fetch('https://api.carbonintensity.org.uk/intensity')
+  const body = await response.json()
+  const reading = body.data[0].intensity
+
+  app.innerHTML = `
+    <p class="reading">
+      <span class="reading__value">${reading.actual}</span>
+      <span class="reading__unit">gCO₂/kWh</span>
+    </p>
+  `
+} catch (error) {
+  console.error(error)
+  app.innerHTML = `<p class="reading">Couldn't reach the grid. Check your connection.</p>`
+}
